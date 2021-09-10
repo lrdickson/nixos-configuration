6d6a5868ecaf5b3dbd4024e3ff7b89f7e26ea56a
Lyndsey Dickson <lyndseyrd@gmail.com>
2021-09-09 21:17:34 -0400

    Clean up server configuration

--- server-configuration.nix
+++ server-configuration.nix
@@ -1,27 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   networking.hostName = "nicole";
   networking.interfaces.eno1.useDHCP = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    docker-compose
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    w3m
-    wget
-  ];
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
