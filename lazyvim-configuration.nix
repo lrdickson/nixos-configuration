529176b3437ea7cbf3ae5926b79d8035b89db0b5
Lyndsey Dickson <lyndseyrd@gmail.com>
2025-06-13 16:54:34 -0400

    Update

--- lazyvim-configuration.nix
+++ lazyvim-configuration.nix
@@ -1,23 +1,40 @@
 { config, lib, pkgs, ... }:
 
+let
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") { };
+in
 {
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    configure = {
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        # loaded on launch
+        start = [ ];
+        # manually loadable by calling `:packadd $plugin-name`
+        opt = [ ];
+      };
+    };
+  };
   environment.systemPackages = with pkgs; [
-    neovim
+    curl
+    gcc
     git
     lazygit
-    gcc
-    curl
-    python3
     lua51Packages.lua
-    lua51Packages.luarocks
     lua51Packages.lua-lsp
-    tree-sitter
+    lua51Packages.luarocks
+    neovim
     nodejs
-
+    python3
+    tree-sitter
 
     # fzf-lua
+    fd
     fzf
     ripgrep
-    fd
+
+    # Formatters
+    stylua
   ];
 }
